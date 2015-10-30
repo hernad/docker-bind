45e4f4c43acf057873e7da5d19920556ab9c1afd
Jasmin Beganović <bjasko@bring.out.ba>
2015-10-30 11:52:54 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:14.04
+MAINTAINER Jasmin Beganović <bjasko@bring.out.ba>
+
+  
+RUN apt-get update -q
+RUN apt-get install -qy bind9  
+
+VOLUME  ["/etc/bind"]
+
+CMD ["/usr/sbin/named", "-g", "-c", "/etc/bind/named.conf", "-u", "bind"]
+
---
+++ run_vpn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+VOLUME_BASE=/data/dns 
+S_HOST=ns-lan
+S_DEV=eth1
+S_DOMAIN=bring.out.ba
+S_HOST_IP=${smtp_ip:-192.168.168.52}
+S_DNS_HOST_IP=${dns_lan_ip:-192.168.168.52}
+CT_NAME=lan-dns
+
+
+sudo ip addr show | grep $S_HOST_IP || \
+sudo ip addr add $S_HOST_IP/24 dev $S_DEV
+
+
+docker rm -f  $CT_NAME
+docker run -d \
+      -v $VOLUME_BASE/$S_HOST.$S_DOMAIN/etc/bind:/etc/bind \
+      --name $CT_NAME \
+      -p $S_HOST_IP:53:53/udp  \
+      bind9
+
